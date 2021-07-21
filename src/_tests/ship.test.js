8d6c48668c4aa98e0456851dd477f1e811d10e8e
mrjackmaster <jacektrg+github@gmail.com>
2021-07-21 23:15:11 +0200

    Basic tests confirming the ship class and the functions work correctly

---
+++ src/_tests/ship.test.js
@@ -0,0 +1,33 @@
+const Ship = require('../ship');
+
+describe('Ship Factory functions', () => {
+	let ship1;
+	test('Creates ship correctly', () => {
+		expect((ship1 = new Ship({ name: 'S', size: 2 }, [2, 3]))).toEqual({
+			name: 'S',
+			size: 2,
+			hits: [],
+			cellsOccupied: [2, 3],
+		});
+	});
+
+	test('Correctly returns size', () => {
+		expect(ship1.getSize).toBe(2);
+	});
+
+	test('Correctly returns hits taken', () => {
+		expect(ship1.getHits).toEqual([]);
+	});
+
+	test('Correctly check whether ship is sunk', () => {
+		expect(ship1.isSunk()).toBe(false);
+	});
+
+	test('Correctly registers shot and gets sunk', () => {
+		ship1.getShot(2);
+		expect(ship1.getHits).toEqual([2]);
+		ship1.getShot(3);
+		expect(ship1.getHits).toEqual([2, 3]);
+		expect(ship1.isSunk()).toBe(true);
+	});
+});
