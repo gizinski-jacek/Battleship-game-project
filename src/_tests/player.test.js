d6241a583ebf3ad83232d8c0cbfe0fef40756983
mrjackmaster <jacektrg+github@gmail.com>
2021-07-22 18:52:50 +0200

    Removing first test for random shots, updating second one for board of size 100cells

--- src/_tests/player.test.js
+++ src/_tests/player.test.js
@@ -43,23 +43,10 @@ describe('Player Factory functions', () => {
 		expect(player.getShotCells).toEqual([11]);
 	});
 
-	// Two ways I came up with to test random AI shots. In the end I will probably go with the second one.
-	test('Testing random AI shot by leaving only available shot to be 24', () => {
-		computer._shotCells = [
-			0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 11, 13, 14, 15, 16, 17, 18,
-			19, 20, 21, 22, 23,
-		];
+	test('Testing random AI shot by forcing the random value to be 20% of board size (100cells)', () => {
+		jest.spyOn(global.Math, 'random').mockReturnValue(0.25);
 		computer.takeShot();
-		expect(computer.getShotCells).toEqual([
-			0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 11, 13, 14, 15, 16, 17, 18,
-			19, 20, 21, 22, 23, 24,
-		]);
-	});
-
-	test('Testing random AI shot by forcing the random value to be 20% of board size (25cells atm)', () => {
-		jest.spyOn(global.Math, 'random').mockReturnValue(0.2);
-		computer.takeShot();
-		expect(computer.getShotCells).toEqual([5]);
+		expect(computer.getShotCells).toEqual([25]);
 		jest.spyOn(global.Math, 'random').mockRestore();
 	});
 });
