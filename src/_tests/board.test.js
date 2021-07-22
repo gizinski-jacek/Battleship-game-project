2a5317c9875c5a50a762ac8de5af252ed2df6477
mrjackmaster <jacektrg+github@gmail.com>
2021-07-22 15:15:34 +0200

    Updating tests with proper getters

--- src/_tests/board.test.js
+++ src/_tests/board.test.js
@@ -2,22 +2,36 @@ const Gameboard = require('../board');
 
 describe('Gameboard Factory functions', () => {
 	let board;
+
 	beforeEach(() => {
 		board = new Gameboard();
 		board.placeShip('shipM', 'hor', 2);
 	});
 
 	test('Correctly created board and placed 3 tile big ship', () => {
-		expect(board.occupiedCells).toEqual([2, 3, 4]);
+		expect(board.getOccupiedCells).toEqual([2, 3, 4]);
+		expect(board.getMissedShotsCells).toEqual([]);
+		expect(board.getShipList).toEqual([
+			{
+				name: 'M',
+				size: 3,
+				hits: [],
+				cellsOccupied: [2, 3, 4],
+			},
+		]);
+	});
+
+	test('Correctly created board and placed 3 tile big ship', () => {
+		expect(board.getOccupiedCells).toEqual([2, 3, 4]);
 	});
 
 	test('Correctly placed additional 2 tile big vertical ship', () => {
 		board.placeShip('shipS', 'ver', 21);
-		expect(board.occupiedCells).toEqual([2, 3, 4, 21, 31]);
+		expect(board.getOccupiedCells).toEqual([2, 3, 4, 21, 31]);
 	});
 
 	test('Registers missed shots', () => {
 		board.receiveShot(5);
-		expect(board.missedShotsCells).toEqual([5]);
+		expect(board.getMissedShotsCells).toEqual([5]);
 	});
 });
