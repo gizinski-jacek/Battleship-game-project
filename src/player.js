1aea8bd1472727681cb035877e1e345a8e0569cf
mrjackmaster <jacektrg+github@gmail.com>
2021-07-23 16:28:36 +0200

    Removed unused myTurn property. Merged takeRandomShot functionality with takeShot.

--- src/player.js
+++ src/player.js
@@ -1,29 +1,21 @@
 class Player {
-	constructor(name, isAI = false, turn = false) {
+	constructor(name, isAI = false) {
 		this._name = name;
 		this._isAI = isAI;
-		this._myTurn = turn;
 		this._shotCells = [];
 	}
 
 	takeShot(cell) {
 		if (this._isAI) {
-			this._shotCells.push(this.takeRandomShot());
+			let random = null;
+			while (random === null || this._shotCells.includes(random)) {
+				random = Math.floor(Math.random() * cell);
+			}
+			this._shotCells.push(random);
+			return random;
 		} else {
 			this._shotCells.push(cell);
 		}
-	}
-
-	takeRandomShot() {
-		let random = null;
-		while (random === null || this._shotCells.includes(random)) {
-			random = Math.floor(Math.random() * 100);
-		}
-		return random;
-	}
-
-	changeTurn() {
-		this._myTurn = !this._myTurm;
 	}
 
 	get getName() {
@@ -32,10 +24,6 @@ class Player {
 
 	get getShotCells() {
 		return this._shotCells;
-	}
-
-	get getMyTurn() {
-		return this._myTurn;
 	}
 }
 
